5983e7fff5ef1876da7fd423d4362852801f6897
allcompva <allcompva@gmail.com>
2022-05-31 07:50:54 -0300

    no message

---
+++ Services/BadecServices.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Web_Api_ejemplo.Entities;
+
+namespace Web_Api_ejemplo.Services
+{
+    public class BadecServices : IBadecServices
+    {
+        public void delete(int nro_bad)
+        {
+            try
+            {
+                Entities.BADEC.delete(nro_bad);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public BADEC getByPk(int nro_bad)
+        {
+            try
+            {
+                return Entities.BADEC.getByPk(nro_bad);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public int insert(BADEC obj)
+        {
+            try
+            {
+                return Entities.BADEC.insert(obj);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public List<BADEC> read()
+        {
+            try
+            {
+                return Entities.BADEC.read();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public List<BADEC> read2()
+        {
+            try
+            {
+                return Entities.BADEC.read2();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public void update(BADEC obj)
+        {
+            try
+            {
+                Entities.BADEC.update(obj);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ Services/IBadecServices.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web_Api_ejemplo.Services
+{
+    public interface IBadecServices
+    {
+        public int insert(Entities.BADEC obj);
+        public void update(Entities.BADEC obj);
+        public void delete(int nro_bad);
+        public List<Entities.BADEC> read();
+        public List<Entities.BADEC> read2();
+        public Entities.BADEC getByPk(int nro_bad);
+    }
+}
